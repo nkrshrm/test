83abb1b1bc5c350ef6dc610a9263056efa3676bf
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-04-15 15:37:10 -0400

    Create deleteauto.ps1

---
+++ deleteauto.ps1
@@ -0,0 +1,2 @@
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /y
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /y
