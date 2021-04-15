7ca9397fe7e7a9ea7e18722555f626e1a0f87df0
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-04-15 15:33:37 -0400

    Delete test.ps1

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$DefaultUsername = "test"
-$DefaultPassword = "test"
-Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
-Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
-Set-ItemProperty $RegPath "AutoLogonCount" -Value "1" -type DWord
-Start-Sleep -Seconds 5 ; Restart-Computer -Force
