3a49f6d3bfac238b45ac3f93f8f8be1c16de895b
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-04-15 15:33:23 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$DefaultUsername = "Insert Username Here"
-$DefaultPassword = "Insert Password Here"
+$DefaultUsername = "test"
+$DefaultPassword = "test"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
