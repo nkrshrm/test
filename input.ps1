d5481d145b0d803b1d0a67289ff0e0521327acbc
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-04-16 16:06:29 -0400

    Create input.ps1

---
+++ input.ps1
@@ -0,0 +1,7 @@
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$DefaultUsername = Read-Host "enter username"; $DefaultPassword = Read-Host "enter password"
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+Set-ItemProperty $RegPath "AutoLogonCount" -Value "1" -type DWord
+Start-Sleep -Seconds 5 ; Restart-Computer -Force
