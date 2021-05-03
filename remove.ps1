0147937df3a27ba9ace5f6096ce8090599573138
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-03 15:58:04 -0400

    Delete remove.ps1

--- remove.ps1
+++
@@ -1,1 +0,0 @@
-Powershell.exe Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsStore*"} | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.name -notlike "*Microsoft.Windows.Photos*"} | Remove-AppxPackage
