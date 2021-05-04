0134a7d73bd7900a0ffe14dbe8e75c8111d51252
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-04 08:23:16 -0400

    Create removenew.ps1

---
+++ removenew.ps1
@@ -0,0 +1,1 @@
+Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsStore*"} | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.name -notlike "*Microsoft.Windows.Photos*"} | Remove-AppxPackage
