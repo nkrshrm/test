17947a5b6534a57b407b2ed76919e916fd6b9d81
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-06 20:33:33 -0400

    Update hopeso.ps1

--- hopeso.ps1
+++ hopeso.ps1
@@ -1,4 +1,4 @@
-$username = "Admin"
-$password = "Admin"
+$username = "test1"
+$password = "test"
 $credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
 Start-Process "C:\Windows\digital-ap-exams-setup-0.9.2.exe" -Credential ($credentials)
