16a20eaf1b70a072f53f938d6afb5b1a144cad3f
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-07 13:28:09 -0400

    Update and rename goodtest.ps1 to goodtest1.ps1

--- goodtest1.ps1
+++ goodtest1.ps1
@@ -1,3 +1,2 @@
-New-Item -Path 'C:\Temp' -ItemType Directory
 Invoke-WebRequest 'https://download.app.collegeboard.org/downloads/digital-ap-exams-setup-0.9.2.exe' -OutFile 'C:Temp\digital-ap-exams-setup-0.9.2.exe'
 Invoke-WebRequest 'https://raw.githubusercontent.com/nkrshrm/test/main/srt.ps1' -OutFile 'C:Temp\srt.ps1'
