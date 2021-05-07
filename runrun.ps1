53bcc30ac0ed34399fe90c3202723475b741e9fc
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-07 09:08:06 -0400

    Update runrun.ps1

--- runrun.ps1
+++ runrun.ps1
@@ -1,5 +1,5 @@
-$username = 'test1'
-$password = 'test'
+$username = 'Admin'
+$password = 'Admin1'
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
 Start-Process -FilePath "C:\Windows\srt.ps1" -Credential $credential
