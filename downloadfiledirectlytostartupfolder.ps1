f58ad16400569f8211753789d583ff93026debf1
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-05-10 10:19:38 -0400

    Update downloadfiledirectlytostartupfolder.ps1

--- downloadfiledirectlytostartupfolder.ps1
+++ downloadfiledirectlytostartupfolder.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest 'https://download.app.collegeboard.org/downloads/digital-ap-exams-setup-0.9.2.exe' -Outfile 'C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
+Invoke-WebRequest 'https://download.app.collegeboard.org/downloads/digital-ap-exams-setup-0.9.2.exe' -Outfile 'C:\Users\Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\digital-ap-exams-setup-0.9.2.exe'
