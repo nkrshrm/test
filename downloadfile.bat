c706f83cd18bb10d099a0f5d40ab6012498fb9d5
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-06-08 15:11:29 -0400

    Create downloadfile.bat

---
+++ downloadfile.bat
@@ -0,0 +1,1 @@
+powershell.exe -command "Invoke-WebRequest https://faronicsdeploystorage.s3.ca-central-1.amazonaws.com/BitDefender/eps_installer_signed.msi -Outfile C:\Temp\eps_installer_signed.msi"
