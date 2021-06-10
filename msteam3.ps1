a4bb7c6cd08d5338371f1cb800ba5db79ec0941f
nkrshrm <79326081+nkrshrm@users.noreply.github.com>
2021-06-10 16:58:50 -0400

    Delete msteam3.ps1

--- msteam3.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest 'https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0x1009&culture=en-ca&country=CA&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64' -Outfile 'C:\Temp\Teams_windows_x64.exe'
-ping -n 30 127.0.0.1
-Start-Process -Wait -FilePath 'C:\Temp\Teams_windows_x64.exe' -ArguementList '/S' -PassThru
